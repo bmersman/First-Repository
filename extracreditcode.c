6f93b9d2c125c45304d875d67b11d25b648d8bd7
bmersman <43159909+bmersman@users.noreply.github.com>
2018-09-19 17:24:52 -0400

    Add files via upload

---
+++ extracreditcode.c
@@ -0,0 +1,20 @@
+// Blake Mersman
+//Assignment 
+//hw
+//Due date
+// description
+
+
+#include <stdio.h>
+#include <math.h> 
+
+int main(void) {
+
+	int STDOUT;
+
+	printf("enter an integer:");
+	scanf("%d", &STDOUT);
+	printf("%d", STDOUT);
+
+	return 0;
+}
